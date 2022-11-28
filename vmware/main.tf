bda8bda397cdc79e4c60a15c77107e47dcab2a23
adworjan <adworjan@redhat.com>
2022-11-28 17:03:50 +0000

    Synching new files to repo

--- vmware/main.tf
+++ vmware/main.tf
@@ -27,11 +27,11 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "/${var.datacenter}/vm/RHEL7_ShadowMan"
+  name          = "/${var.datacenter}/vm/RHEL8_ShadowMan"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-resource "vsphere_virtual_machine" "server1" {
-  name             = "server1.shadowman.dev"
+resource "vsphere_virtual_machine" "vmware" {
+  name             = "vmware.shadowman.dev"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
   folder           = "Discovered virtual machine"
@@ -50,7 +50,7 @@ resource "vsphere_virtual_machine" "server1" {
   wait_for_guest_ip_timeout  = -1
 
   disk {
-    label            = "server1"
+    label            = "vmware"
     thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
     size             = data.vsphere_virtual_machine.template.disks.0.size
   }
@@ -62,7 +62,7 @@ resource "vsphere_virtual_machine" "server1" {
   }
 }
 
-output "vm_name_server1" {
-  value = vsphere_virtual_machine.server1.name
+output "vm_name_vmware" {
+  value = vsphere_virtual_machine.vmware.name
 }
 

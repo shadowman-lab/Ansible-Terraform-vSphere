365138f2f128f3780755c7499fcdf9cccc7aa833
adworjan <adworjan@redhat.com>
2023-07-31 18:15:58 +0000

    Synching new files to repo

--- vmware/main.tf
+++ vmware/main.tf
@@ -27,7 +27,7 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "/${var.datacenter}/vm/RHEL9_ShadowMan"
+  name          = "/${var.datacenter}/vm/RHEL7_ShadowMan"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 resource "vsphere_virtual_machine" "alex" {

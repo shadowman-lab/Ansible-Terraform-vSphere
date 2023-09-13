ecbb6f4f443a0abc91f4dca445ad71439d053b39
adworjan <adworjan@redhat.com>
2023-09-13 12:45:13 +0000

    Synching new files to repo

--- vmware/main.tf
+++ vmware/main.tf
@@ -27,11 +27,11 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "/${var.datacenter}/vm/RHEL7_ShadowMan"
+  name          = "/${var.datacenter}/vm/RHEL9_ShadowMan"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-resource "vsphere_virtual_machine" "kjztf" {
-  name             = "kjztf.shadowman.dev"
+resource "vsphere_virtual_machine" "s1" {
+  name             = "s1.shadowman.dev"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
   folder           = "Discovered virtual machine"
@@ -50,7 +50,7 @@ resource "vsphere_virtual_machine" "kjztf" {
   wait_for_guest_ip_timeout  = -1
 
   disk {
-    label            = "kjztf"
+    label            = "s1"
     thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
     size             = data.vsphere_virtual_machine.template.disks.0.size
   }
@@ -62,7 +62,7 @@ resource "vsphere_virtual_machine" "kjztf" {
   }
 }
 
-output "vm_name_kjztf" {
-  value = vsphere_virtual_machine.kjztf.name
+output "vm_name_s1" {
+  value = vsphere_virtual_machine.s1.name
 }
 
